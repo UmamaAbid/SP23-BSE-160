a45ec52b0545ea6020f1b6ff731c4579bc8df03c
UmamaAbid <136912754+UmamaAbid@users.noreply.github.com>
2024-12-18 17:07:42 +0500

    Create server.js

---
+++ labfinal/backend/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+const app = express()
+const port = 4000
+//middleware
+app.use(express.json())
+app.use(cors())
+app.get("/", (req,res)=>{
+    res.send("api working")
+})
+
+app.listen(port, ()=>{
+    console.log(`server started on http://localhost:${port}`)
+})
